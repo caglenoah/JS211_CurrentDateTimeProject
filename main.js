5f4c29dc4fac21433f577d7a4e9c8bcbfd86a379
caglenoah <caglenoah@gmail.com>
2023-07-04 18:50:58 -0600

    first commit, will complete by end of day! just getting things turned in

--- main.js
+++ main.js
@@ -8,40 +8,96 @@
 console.log(new Date)
 
 const displayDate = () => {
-  const currentDate = new Date()
 
+  const currentDate = new Date();
+  
   document.getElementById("display-element").innerHTML = currentDate;
 }
- 
 
 // Write a JavaScript program to convert a number to a string.
+const numberToString = () => {
+  let x = 2423253435;
+  
+  x = x.toString();
 
-
+  console.log("The type of x is " + typeof x)
+  //send to DOM
+  document.getElementById("num-str").innerHTML = x;
+  
+}
 
 // Write a JavaScript program to convert a string to the number.
-
-
-
-// Write a JavaScript program that takes in different datatypes and prints out whether they are a:
-  // * Boolean
-  // * Null
-  // * Undefined
-  // * Number
-  // * NaN
-  // * String
+const strToNum = () => {
+  let y = '56789'
   
+  let x = parseInt(y)
 
+  console.log("The typeof x is " + typeof x)
+  document.getElementById("str-num").innerHTML = x;
+
+}
+
+//re-try!!!
+// // Write a JavaScript program that takes in different datatypes and prints out whether they are a:
+// const typeOfVars = () => {
+    
+//   //  Boolean
+//  let b = true 
+//  console.log("b is " + typeof b)
   
-// Write a JavaScript program that adds 2 numbers together.
+//  ////  Null
+//   let d = null
+//   console.log("d is " + d)
+  
+//  //  Undefined
+//   let c;
+//   console.log("c is " + typeof c)
+  
+// //   Numbers
+//   let e = 123 
+//   console.log("e is a " + typeof e)
+  
+// //   NaN
+//   let a = 25 * '100f' 
+//   console.log("a is " + a)
+  
+// //   String
+//   let f = '123'
+//   console.log("f is type " + typeof f)
 
-
-
+//   getComputedStyle.ElementById("var-types").innerHTML = "The typeof a is " + typeof a;
+// }
+  
+  
+  
+  //re-try
+  // Write a JavaScript program that adds 2 numbers together.
+const add = (numA, numB) = (num) => {
+  const sum = numA + numB;
+  return sum;
+  console.log("the sum of two numbers is " + sum)
+  } 
+  
+  //re-try
 // Write a JavaScript program that runs only when 2 things are true.
-
+const twoAreTrue = (a,b) => {
+  if {
+    a = true;
+    b = true;
+    document.getElementById("two-tru").innerHTML=twoAreTrue()
+  }
+    else {
+    document.getElementById("two-tru").innerHTML = "try again!";
+    }
+    
+}
 
 
 // Write a JavaScript program that runs when 1 of 2 things are true.
-
+const x = love 
+if{
+  
+}
 
 
 // Write a JavaScript program that runs when both things are not true.  
